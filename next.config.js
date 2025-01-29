d3f101b8763ec6d639e1da91af1f1ba279680247
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2025-01-29 12:55:44 -0500

    feat: UNIC-00 add cors config 2

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: process.env.NEXT_PUBLIC_KC_AUTH_SERVER_URL },
+          { key: 'Access-Control-Allow-Origin', value: 'https://auth.qa.unic.ferlab.bio' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
