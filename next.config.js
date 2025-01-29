f90f4a3d5741bc0e6b567c517f90333966a04a95
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2025-01-29 12:50:23 -0500

    feat: UNIC-00 add cors config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,23 @@ const nextConfig = {
   /* config options here */
   reactStrictMode: false,
   output: 'standalone', // Optimized for deployment on Docker
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: process.env.NEXT_PUBLIC_KC_AUTH_SERVER_URL },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -5,6 +5,8 @@ import KeycloakProvider from 'next-auth/providers/keycloak';
 import config from '@/config';
 
 const { KC_AUTH_SERVER_URL, KC_CLIENT_ID, KC_REALM } = config;
+const COOKIES_LIFE_TIME = 24 * 60 * 60;
+const COOKIE_PREFIX = process.env.NODE_ENV === 'production' ? '__Secure-' : '';
 
 const keycloakConfig = {
   clientId: KC_CLIENT_ID,
@@ -60,6 +62,78 @@ export const authOptions: AuthOptions = {
       }
 
       return _token;
+    },
+  },
+  // events: {
+  //   async signOut({ token }) {
+  //     const logOutUrl = new URL(
+  //       `${process.env.KEYCLOACK_ISSUER}/protocol/openid-connect/logout`,
+  //     );
+  //     logOutUrl.searchParams.set('id_token_hint', token.id_token!);
+  //
+  //     await fetch(logOutUrl);
+  //   },
+  // },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: true,
+  pages: {
+    // signIn: '/',
+  },
+  cookies: {
+    sessionToken: {
+      name: `${COOKIE_PREFIX}next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true,
+      },
+    },
+    callbackUrl: {
+      name: `${COOKIE_PREFIX}next-auth.callback-url`,
+      options: {
+        sameSite: 'lax',
+        path: '/',
+        secure: true,
+      },
+    },
+    csrfToken: {
+      name: `${COOKIE_PREFIX}next-auth.csrf-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true,
+      },
+    },
+    pkceCodeVerifier: {
+      name: `${COOKIE_PREFIX}next-auth.pkce.code_verifier`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true,
+        maxAge: COOKIES_LIFE_TIME,
+      },
+    },
+    state: {
+      name: `${COOKIE_PREFIX}next-auth.state`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true,
+        maxAge: COOKIES_LIFE_TIME,
+      },
+    },
+    nonce: {
+      name: `${COOKIE_PREFIX}next-auth.nonce`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true,
+      },
     },
   },
 };
