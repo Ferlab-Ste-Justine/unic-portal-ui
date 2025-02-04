ee545d33b7c351413ec69dcf4622af7fa206dd0a
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2025-02-04 16:01:35 -0500

    feat: UNIC-42 remove fake stats

--- src/components/DataRelease/DataRelease.tsx
+++ src/components/DataRelease/DataRelease.tsx
@@ -37,13 +37,6 @@ const DataRelease = ({ className = '' }: IDataReleaseProps) => {
           className={styles.dataReleaseStatsLabel}
           subLabel={<span className={styles.dataReleaseStatsSubLabel}>{intl.get('entities.project.Projects')}</span>}
         />
-        <MultiLabel
-          iconPosition={MultiLabelIconPositionEnum.Top}
-          label={numberFormat(projects_count)}
-          Icon={<ReadOutlined className={styles.dataReleaseIcon} />}
-          className={styles.dataReleaseStatsLabel}
-          subLabel={<span className={styles.dataReleaseStatsSubLabel}>{'faketitlefortest'}</span>}
-        />
       </Col>
       <Col flex='auto' className={styles.colDataReleaseContainer}>
         <MultiLabel
