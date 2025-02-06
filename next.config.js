547500cc20e287544660ee8dad5156ffb457607c
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2025-02-06 13:04:14 -0500

    feat: UNIC-00 add css-loader old compatibility, switch mode pure to local

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   /* config options here */
   reactStrictMode: false,
   output: 'standalone', // Optimized for deployment on Docker
+  webpack: (config) => {
+    config.module.rules.forEach((rule) => {
+      if (rule.oneOf) {
+        rule.oneOf.forEach((subRule) => {
+          if (Array.isArray(subRule.use)) {
+            subRule.use.forEach((loader) => {
+              if (typeof loader === 'object' && loader.loader?.includes('css-loader') && loader.options?.modules) {
+                loader.options.modules.mode = 'local'; // Change from "pure" to "local"
+              }
+            });
+          }
+        });
+      }
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
