2c9f8477870d4378ab3633f08c74a95cb6a1d232
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2025-02-17 09:16:35 -0500

    feat: UNIC-38 add tab titles

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -21,7 +21,6 @@ const en = {
     },
     resource: {
       resource: 'resource',
-      Resource: 'Resource',
       resources: 'resources',
       Resource: 'Resource',
       Resources: 'Resources',
