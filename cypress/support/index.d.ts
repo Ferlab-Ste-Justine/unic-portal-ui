a95454a97e888d6843091042f3628f8548b6d9dd
kstonge <karine.st-onge.hsj@ssss.gouv.qc.ca>
2025-02-18 10:52:11 -0500

    feat: UNICWEB-80 Add Resources Table Cypress tests (related to UNICWEB-37)

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -6,7 +6,14 @@ declare namespace Cypress {
     clickAndWait(options?: Partial<ClickOptions>): Chainable<Element>;
     login(): cy & CyEventEmitter;
     logout(): cy & CyEventEmitter;
+    resetColumns(table_id?: string): cy & CyEventEmitter;
+    showColumn(column: string|RegExp): cy & CyEventEmitter;
+    sortTableAndIntercept(column: string|RegExp, nbCalls: number): cy & CyEventEmitter;
+    sortTableAndWait(column: string|RegExp): cy & CyEventEmitter;
     typeAndIntercept(selector: string, text: string, methodHTTP: string, routeMatcher: string, nbCalls: number): cy & CyEventEmitter;
+    validatePaging(total: string|RegExp, eqSelect: number): cy & CyEventEmitter;
+    validateTableFirstRow(expectedValue: string|RegExp, eq: number, hasCheckbox: boolean = false): cy & CyEventEmitter;
+    validateTableResultsCount(expectedCount: string|RegExp, shouldExist: boolean = true): cy & CyEventEmitter;
     visitAndIntercept(url: string, methodHTTP: string, routeMatcher: string, nbCalls: number): cy & CyEventEmitter;
     visitCatalog(): cy & CyEventEmitter;
     waitWhileSpin(ms: number): cy & CyEventEmitter;
