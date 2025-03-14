a08ea852185790f087bfa16e137d06c128d5c9f1
kstonge <karine.st-onge.hsj@ssss.gouv.qc.ca>
2025-03-14 10:35:47 -0400

    feat: UNICWEB-71 Adjust Cypress tests

--- cypress/e2e/Consultation/Header.cy.ts
+++ cypress/e2e/Consultation/Header.cy.ts
@@ -10,9 +10,8 @@ describe('Header', () => {
   it('Vérifier les informations affichées', () => {
     cy.get('[class*="Header_headerBtnActive"]').contains('Home').should('exist');
     cy.get('[class*="Header_headerBtn"]').eq(1).contains('Catalog').should('exist');
-    cy.get('[class*="Header_userName"]').eq(0).contains('Resources').should('exist');
     cy.get('[class*="Header_userAvatar"]').contains('CN').should('exist');
-    cy.get('[class*="Header_userName"]').eq(1).contains('cypress').should('exist');
+    cy.get('[class*="Header_userName"]').contains('cypress').should('exist');
     cy.get('[class*="Header_langButton"]').contains('FR').should('exist');
   });
 
@@ -25,12 +24,6 @@ describe('Header', () => {
   it('Valider les liens disponibles - Catalog', () => {
     cy.get('[class*="Header_headerBtn"]').eq(1).clickAndWait();
     cy.get('[class*="PageLayout_titlePage"]').contains('UnIC Catalog').should('exist');
-  });
-
-  it('Valider les liens disponibles - Contact', () => {
-    cy.get('[class*="Header_menuTrigger"] [class*="anticon-down"]').eq(0).clickAndWait();
-    cy.get('[data-menu-id*="contact"]').find('a')
-      .should('have.attr', 'href', 'mailto:support@unic.ca');
   });
 
   it('Valider les liens disponibles - FR', () => {
