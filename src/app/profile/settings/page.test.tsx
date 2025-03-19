40b3770f8483c87fa21bf012362426889d54dadb
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2025-03-19 15:52:22 -0400

    feat: UNIC-34 add tests

--- src/app/profile/settings/page.test.tsx
+++ src/app/profile/settings/page.test.tsx
@@ -1,132 +1,40 @@
-import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
 
-import { fireEvent, render, screen } from '@testing-library/react';
-import { useRouter } from 'next/navigation';
-import { Provider, useDispatch } from 'react-redux';
-import configureStore from 'redux-mock-store';
+import ProfileSettingsPage from '@/app/profile/settings/page';
+import { useAxios } from '@/lib/axios';
 
-import { useAuth } from '@/app/api/auth/useAuth';
-import LoginPage from '@/app/login/page';
-import { globalActions } from '@/store/global';
+jest.mock('@/components/ProfilePage/Cards/DeleteCard', () => jest.fn(() => <div>Delete Card</div>));
+jest.mock('@/components/ProfilePage/Cards/FunctionCard', () => jest.fn(() => <div>Function Card</div>));
+jest.mock('@/components/ProfilePage/Cards/IdentificationCard', () => jest.fn(() => <div>Identification Card</div>));
+jest.mock('@/components/ProfilePage/Cards/ResearchDomainCard', () => jest.fn(() => <div>Research Domain Card</div>));
 
-jest.mock('next/navigation', () => ({
-  useRouter: jest.fn(),
-}));
-jest.mock('@/app/api/auth/useAuth');
-jest.mock('@/store/global', () => ({
-  globalActions: {
-    changeLang: jest.fn(),
-  },
-  useLang: jest.fn(() => 'EN'),
-  useGlobals: () => ({
-    stats: {},
-  }),
-  fetchStats: jest.fn(),
-}));
 jest.mock('react-intl-universal', () => ({
   get: jest.fn((key) => key),
 }));
-jest.mock('react-redux', () => ({
-  ...jest.requireActual('react-redux'),
-  useDispatch: jest.fn(),
+jest.mock('@/lib/axios', () => ({
+  useAxios: jest.fn(),
 }));
 
-describe('LoginPage Component', () => {
-  const initialState = {
-    global: {
-      lang: 'EN',
-    },
-  };
-
-  const mockStore = configureStore();
-  const store = mockStore(initialState);
-  const mockPush = jest.fn();
-  const mockLogin = jest.fn();
-  const mockDispatch = jest.fn();
-
-  (useAuth as jest.Mock).mockReturnValue({
-    isAuthenticated: false,
-    login: mockLogin,
-  });
-  (useRouter as jest.Mock).mockReturnValue({
-    push: mockPush,
-  });
-  (useDispatch as unknown as jest.Mock).mockReturnValue(mockDispatch);
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it('renders the login page correctly', () => {
-    render(
-      <Provider store={store}>
-        <LoginPage />
-      </Provider>,
-    );
-
-    expect(mockDispatch).toHaveBeenCalled();
-    expect(screen.getByAltText('login-img')).toBeInTheDocument();
-    expect(screen.getByAltText('logo')).toBeInTheDocument();
-    expect(screen.getByText('screen.loginPage.title')).toBeInTheDocument();
-    expect(screen.getByText('screen.loginPage.subTitle')).toBeInTheDocument();
-    expect(screen.getByText('components.dataRelease.title')).toBeInTheDocument();
-    expect(screen.getByText('global.participants')).toBeInTheDocument();
-    expect(screen.getByText('entities.project.Projects')).toBeInTheDocument();
-    expect(screen.getByText('entities.source_system.Source_systems')).toBeInTheDocument();
-    expect(screen.getByText('entities.variable.Variables')).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'screen.loginPage.login' })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'screen.loginPage.signup' })).toBeInTheDocument();
-  });
-
-  it('calls login function when login button is clicked', () => {
-    render(
-      <Provider store={store}>
-        <LoginPage />
-      </Provider>,
-    );
-
-    const loginButton = screen.getByRole('button', { name: 'screen.loginPage.login' });
-    fireEvent.click(loginButton);
-
-    expect(mockLogin).toHaveBeenCalledTimes(1);
-  });
-
-  it('calls login function when signup button is clicked', () => {
-    render(
-      <Provider store={store}>
-        <LoginPage />
-      </Provider>,
-    );
-
-    const signupButton = screen.getByRole('button', { name: 'screen.loginPage.signup' });
-    fireEvent.click(signupButton);
-    expect(mockLogin).toHaveBeenCalledTimes(1);
-  });
-
-  it('switches language when language button is clicked', () => {
-    render(
-      <Provider store={store}>
-        <LoginPage />
-      </Provider>,
-    );
-
-    const languageButton = screen.getByRole('button', { name: 'FR' });
-    fireEvent.click(languageButton);
-    expect(globalActions.changeLang).toHaveBeenCalledTimes(1);
-  });
-
-  it('redirects to home when user is authenticated', () => {
-    (useAuth as jest.Mock).mockReturnValue({
-      isAuthenticated: true,
-      login: mockLogin,
+describe('ProfileSettingsPage', () => {
+  it('renders ProfileSettingsPage and child components correctly', async () => {
+    // Mock API response
+    (useAxios as jest.Mock).mockReturnValueOnce({
+      data: {
+        roleOptions: [{ value: 'admin', label: 'Admin' }],
+        researchDomainOptions: [{ value: 'AI', label: 'AI' }],
+      },
     });
 
-    render(
-      <Provider store={store}>
-        <LoginPage />
-      </Provider>,
-    );
+    render(<ProfileSettingsPage />);
 
-    expect(mockPush).toHaveBeenCalledWith('/');
+    // Wait for API data to be processed
+    await waitFor(() => expect(useAxios).toHaveBeenCalledTimes(1));
+
+    // Check if ProfileSettings and mock child components render
+    expect(screen.getByText('screen.profileSettings.title')).toBeInTheDocument();
+    expect(screen.getByText('Identification Card')).toBeInTheDocument();
+    expect(screen.getByText('Function Card')).toBeInTheDocument();
+    expect(screen.getByText('Research Domain Card')).toBeInTheDocument();
+    expect(screen.getByText('Delete Card')).toBeInTheDocument();
   });
 });
