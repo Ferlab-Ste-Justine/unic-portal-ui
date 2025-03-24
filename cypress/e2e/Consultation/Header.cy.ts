d850ee1f9847b8a2b2b72dd77b2c17e57284d975
kstonge <karine.st-onge.hsj@ssss.gouv.qc.ca>
2025-03-24 15:48:55 -0400

    feat: UNICWEB-134 AdjustCypress tests

--- cypress/e2e/Consultation/Header.cy.ts
+++ cypress/e2e/Consultation/Header.cy.ts
@@ -8,21 +8,21 @@ beforeEach(() => {
 
 describe('Header', () => {
   it('Vérifier les informations affichées', () => {
-    cy.get('[class*="Header_headerBtnActive"]').contains('Home').should('exist');
-    cy.get('[class*="Header_headerBtn"]').eq(1).contains('Catalog').should('exist');
+    cy.get('[class*="Header_logo"]').should('exist');
+    cy.get('[class*="Header_headerBtn"]').contains('Catalog').should('exist');
     cy.get('[class*="Header_userAvatar"]').contains('CN').should('exist');
     cy.get('[class*="Header_userName"]').contains('cypress').should('exist');
     cy.get('[class*="Header_langButton"]').contains('FR').should('exist');
   });
 
-  it('Valider les liens disponibles - Home', () => {
+  it('Valider les liens disponibles - Logo', () => {
     cy.visitCatalog();
-    cy.get('[class*="Header_headerBtn"]').eq(0).clickAndWait();
+    cy.get('[class*="Header_logo"]').clickAndWait();
     cy.get('[class*="PageLayout_titlePage"]').contains('UnIC data portal').should('exist');
   });
 
   it('Valider les liens disponibles - Catalog', () => {
-    cy.get('[class*="Header_headerBtn"]').eq(1).clickAndWait();
+    cy.get('[class*="Header_headerBtn"]').eq(0).clickAndWait();
     cy.get('[class*="PageLayout_titlePage"]').contains('UnIC Catalog').should('exist');
   });
 
