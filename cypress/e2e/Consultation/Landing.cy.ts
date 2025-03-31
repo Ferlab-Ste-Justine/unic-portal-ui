abc0c22a5f12a15dd07a0c0f155a33cfbe7dfda8
kstonge <karine.st-onge.hsj@ssss.gouv.qc.ca>
2025-03-31 07:44:08 -0400

    feat: UNICWEB-145 Adjust Cypress tests

--- cypress/e2e/Consultation/Landing.cy.ts
+++ cypress/e2e/Consultation/Landing.cy.ts
@@ -28,7 +28,7 @@ describe('Page Landing - Vérifier les informations affichées', () => {
 
   it('Titre', () => {
     cy.get('[class*="page_loginTitle"]').contains(/(Portail de données de l'Univers Informationnel du CHU Sainte-Justine.|Data portal of Univers Informationnel du CHU Sainte-Justine.)/).should('exist');
-    cy.get('[class*="page_loginSubTitle"]').contains(/(Explorez le catalogue de données, gérez vos projets et accédez à vos jeux de données.|Explore the data catalog, manage your projects, and access your datasets.)/).should('exist');
+    cy.get('[class*="page_loginSubTitle"]').should('not.exist');
   });
 
   it('Boutons', () => {
